b033c8f32a855c38714d65527542744ee82f94d5
Yifei Zhang <yifei7.zhang@gmail.com>
2020-01-15 19:58:14 -0500

    exploring modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,11 @@
-const fs = require("fs");
+const utils = require("./utils.js");
+const notes = require("./notes.js");
 
-// fs.writeFileSync("notes.txt", "My name is Yifei.");
+const num = utils.add(2, 3);
 
-// Challenge: Append a message to notes.txt
-fs.appendFileSync("notes.txt", "\nThis is a cool feature!");
+console.log(num);
+console.log(utils.name);
+console.log(utils);
+console.log(notes());
+
+// Define and use a function in a new file
---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+console.log("utils.js");
+
+const name = "Yifei";
+
+const add = (a, b) => {
+  return a + b;
+};
+
+module.exports = { add, name };
